ece53ff9a9414cdcf6355c0a6c596b551bc47782
NINE-J <rnwo47@gmail.com>
2024-09-09 11:25:21 +0900

    [M] Test JSDoc

--- src/components/DummyList.tsx
+++ src/components/DummyList.tsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from 'react';
 
-interface user {
+/**
+ * 사용자 정보를 나타내는 인터페이스
+ *
+ * @interface User
+ * @property {string} id - 사용자의 고유 식별자
+ * @property {string} name - 사용자의 이름
+ * @property {string} email - 사용자의 이메일 주소
+ */
+interface User {
   id: string;
   name: string;
   email: string;
 }
 
 const DummyList = () => {
-  const [users, setUsers] = useState<user[]>();
+  const [users, setUsers] = useState<User[]>();
 
   useEffect(() => {
     const apiUrl = 'http://127.0.0.1:5000';
